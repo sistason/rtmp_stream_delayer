7e3ffda2d43e6968079138b2e7fa9366ab47be66
Kai <sistason@sistason.de>
2020-05-14 19:18:28 +0200

    Complete rewrite to OOP and python3
    Added cli-arguments for logging
    Moved the stream-directory to an cli-argument
    Added PID_FILE to solve concurrency
    Added RTMP_EXECUTABLE, to allow for different ffmpeg-stuff
    Rewrite in py3

--- delayer_settings.py
+++ delayer_settings.py
@@ -1,6 +1,6 @@
-STREAM_DIR = '/tmp/nginx'
 STREAM_DESTINATION = 'rtmp://127.0.0.1:1337/live'
-DELAY = 120     #Seconds
-SINGLE = False  #Set to true to just delay a single stream and exit, no reconnecting/backup-streams
-BACKUPSTREAM_SHORT = '' #Show while intermission
-BACKUPSTREAM_LONG = ''  #Show while longer downtime
+DELAY = 120     # Seconds
+SINGLE = False  # Set to true to just delay a single stream and exit, no reconnecting/backup-streams
+BACKUPSTREAM_SHORT = '' # Show while intermission
+BACKUPSTREAM_LONG = ''  # Show while longer downtime
+FFMPEG_EXECUTABLE = "ffmpeg"   # Use avconv if you need
--- rtmp_stream_delayer.py
+++ rtmp_stream_delayer.py
@@ -1,105 +1,193 @@
-#!/usr/bin/env python
-#
+#!/usr/bin/env python3
 
 import subprocess
-import os, sys
+import os, sys, time
+import logging
 
-import time
 
-from delayer_settings import BACKUPSTREAM_SHORT, BACKUPSTREAM_LONG, STREAM_DIR, STREAM_DESTINATION, DELAY, SINGLE
+logger = logging.getLogger(__name__)
 
-def check_running():
-    """ Only one stream at a time """
 
-    all_procs = subprocess.check_output(['ps', 'aux'])
-    if all_procs.count(" python rtmp_stream_delayer.py") > 1:
-        return True
+class StreamDelayer:
+    def __init__(self, stream_directory, stream_destination, backupstream_short, backupstream_long,
+                 delay=300, single=False, ffmpeg_exe="ffmpeg"):
+        self.stream_directory = stream_directory
 
-def cleanup(streams):
-    """ keep the newest stream, if multiple streams are present """
+        self.stream_destination = stream_destination
+        self.backupstream_short = backupstream_short
+        self.backupstream_long = backupstream_long
+        self.delay = delay
+        self.single = single
+        self.ffmpeg_exe = ffmpeg_exe
 
-    current_date = time.time()
-    nearest = min(streams, key=lambda f: abs(current_date - get_date_from_file(f)))
+        self.stream = self._wait_for_stream()
 
-    for stream in streams:
-        if stream == nearest:
-            continue
+    def _wait_for_stream(self):
+        streams = self._get_streams_in_directory()
+        while not streams:
+            time.sleep(0.5)
+            streams = self._get_streams_in_directory()
+
+        logging.debug("Found Streams: {}".format(', '.join(streams)))
+
+        return self._keep_only_latest(streams)
+
+    def _get_streams_in_directory(self):
+        return [os.path.join(self.stream_directory, _i)
+                for _i in os.listdir(self.stream_directory) if _i.endswith('.flv')]
+
+    def _keep_only_latest(self, streams):
+        """ keep the newest stream, if multiple streams are present """
+
+        if len(streams) <= 1:
+            return streams[0]
+
+        nearest = max(streams, key=self.get_date_from_file)
+        streams.remove(nearest)
+
+        for stream in streams:
+            try:
+                os.remove(os.path.join(self.stream_directory, stream))
+            except:
+                pass
+
+        return os.path.join(self.stream_directory, nearest)
+
+    def _get_oldest_stream(self):
+        streams = self._get_streams_in_directory()
+        if streams:
+            return os.path.join(self.stream_directory, min(streams, key=self.get_date_from_file))
+
+    def delay_stream(self):
+        """ Delay a stream.
+
+        Delay the first stream, when it ends check for new files"""
+        stream_date = self.get_date_from_file(self.stream)
+        logging.debug('Delaying stream for {:.0f} seconds...'.format(stream_date + DELAY - time.time()))
+        while time.time() < stream_date + DELAY:
+            time.sleep(0.5)
+
+        self.publish_stream()
+        if SINGLE:
+            # finish if this is just a one-shot
+            return
+
+        self.stream = self._get_oldest_stream()
+        while self.stream:
+            # If there are more files, it means the stream was interrupted by error, so show wait-stream and continue
+            stream_date = self.get_date_from_file(self.stream)
+
+            wait_time = (stream_date + DELAY) - time.time()
+            self.publish_wait(wait_time)
+
+            self.publish_stream()
+
+            self.stream = self._get_oldest_stream()
+
+    def publish_stream(self):
+        """ Publish the current stream.
+
+        Returns True on ok, returns False if crashed"""
+
+        logger.debug('Publishing stream {}'.format(self.stream))
+        return_code = True
         try:
-            os.remove(os.path.join(STREAM_DIR, stream))
-        except:
-            pass
-    return [os.path.join(STREAM_DIR, nearest)]
+            subprocess.check_output(
+                [self.ffmpeg_exe, '-re', '-i', self.stream, '-codec', 'copy', '-f', 'flv', self.stream_destination])
+        except Exception as e:
+            logger.error('Publish stream threw exception {}'.format(e))
+            return_code = False
+        finally:
+            try:
+                os.remove(self.stream)
+            except:
+                pass
+        return return_code
 
-def publish_stream(filename):
-    """ Publish the current stream. 
-    
-    Returns True on ok, returns False if crashed and needs reconnects """
+    def publish_wait(self, wait_time):
+        """ Publish waiting/backup streams, when main stream is down.
 
-    print 'Publishing stream', filename
-    ret = False
-    try:
-        #TODO: When is the stream finished or when is it crashed?
-        subprocess.check_output('avconv -re -i {0} -codec copy -f flv {1}'.format(filename, STREAM_DESTINATION).split())
-        ret = False
-    except:
-        ret = False
-    try:
-        os.remove(filename)
-    except:
-        pass
-    return ret
+        Depending on the waiting time, a short outage or 'Problems' or whatever"""
+        if wait_time < 10:
+            if not self.backupstream_short:
+                time.sleep(wait_time)
+            else:
+                subprocess.check_output([self.ffmpeg_exe, '-re',
+                                         '-i', self.backupstream_short, '-codec', 'copy', '-f', 'flv',
+                                         self.stream_destination])
+        else:
+            if not self.backupstream_long:
+                time.sleep(wait_time)
+            else:
+                subprocess.check_output([self.ffmpeg_exe, '-re',
+                                         '-i', self.backupstream_long, '-codec', 'copy', '-f', 'flv',
+                                         self.stream_destination])
 
-def publish_wait(wait_time):
-    """ Publish waiting/backup streams, when main stream is down.
+    @staticmethod
+    def get_date_from_file(file_):
+        """ nginx saves streams as: test-%s.flv """
+        try:
+            return int(file_.split('-')[-1].split('.')[0])
+        except ValueError:
+            return 0
 
-    Depending on the waiting time, a short outage or 'Problems' or whatever"""
-    if wait_time < 10:
-        problems_short = BACKUPSTREAM_SHORT
-        subprocess.check_output('avconv -re -i {0} -codec copy -f flv {1}'.format(problems_short, STREAM_DESTINATION).split())
-    else:
-        problems_big = BACKUPSTREAM_LONG
-        subprocess.check_output('avconv -re -i {0} -codec copy -f flv {1}'.format(problems_big, STREAM_DESTINATION).split())
-
-def get_date_from_file(file_):
-    """ nginx saves streams as: test-%s.flv """
-    try:
-        return int(file_.split('-')[-1].split('.')[0])
-    except ValueError:
-        return 0
 
 if __name__ == '__main__':
-    # One to rule them all.
-    if check_running():
+    def parse_args():
+        import argparse
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-v', '--verbose', action="store_true")
+        parser.add_argument('-q', '--quiet', action="store_true")
+        parser.add_argument('stream_dir')
+
+        _args = parser.parse_args()
+        return _args
+
+    def set_loglevel(args):
+        log_level = logging.INFO
+        if args.verbose:
+            log_level -= 10
+        if args.quiet:
+            log_level += 20
+        logging.basicConfig(level=log_level)
+
+    def check_running(PID_FILE):
+        """ Only one stream at a time """
+        try:
+            with open(PID_FILE) as f:
+                pid = f.read().strip()
+        except FileNotFoundError:
+            pid = ""
+
+        if pid.isnumeric():
+            """ Check for the existence of a unix pid by sending signal 0 """
+            try:
+                os.kill(int(pid), 0)
+            except OSError:
+                pass
+            else:
+                logger.error('stream_delayer already running (PID-file at {} says it is pid={}!'.format(PID_FILE, pid))
+                return True
+
+        with open(PID_FILE, 'w') as f:
+            f.write(str(os.getppid()))
+
+    args = parse_args()
+    set_loglevel(args)
+
+    PID_FILE = os.path.join(args.stream_dir, "stream_delayer_pid")
+    if check_running(PID_FILE):
         sys.exit(0)
 
-    stream = [_i for _i in os.listdir(STREAM_DIR) if _i.endswith('.flv')]
-    while not stream:
-        time.sleep(0.5)
-        stream = [_i for _i in os.listdir(STREAM_DIR) if _i.endswith('.flv')]
+    try:
+        while True:
+            from delayer_settings import BACKUPSTREAM_SHORT, BACKUPSTREAM_LONG, \
+                STREAM_DESTINATION, DELAY, SINGLE, FFMPEG_EXECUTABLE
+            streamer = StreamDelayer(args.stream_dir, STREAM_DESTINATION, BACKUPSTREAM_SHORT, BACKUPSTREAM_LONG,
+                                     delay=DELAY, single=SINGLE, ffmpeg_exe=FFMPEG_EXECUTABLE)
+            streamer.delay_stream()
 
-    if len(stream) > 1:
-        stream = cleanup(stream)
-
-    first = True
-    while stream:
-        stream = stream[0]
-        stream_date = get_date_from_file(stream)
-
-        if first:
-            # On first run, just delay the begin of the stream
-            print (stream_date+DELAY) - time.time()
-            while time.time() < stream_date+DELAY:
-                time.sleep(0.5)
-        else:
-            # On next runs, which are reconnects, show offline/backup-stream
-            wait_time = (stream_date+DELAY) - time.time()
-            publish_wait(wait_time)
-
-        if publish_stream(stream):
-            # If the stream is just finished, our work here is done
-            sys.exit(0)
-        if SINGLE:
-            sys.exit(0)
-        
-        stream = sorted([_i for _i in os.listdir(STREAM_DIR) if _i.endswith('.flv')])
-        first = False
+    except KeyboardInterrupt:
+        pass
+    finally:
+        os.remove(PID_FILE)
